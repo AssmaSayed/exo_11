759154dcaaf3b60a3484543f09996071c1221796
unknown <assmasayed19@gmail.com>
2021-01-19 12:37:18 +0100

    exo 11 fini

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import React,{useState} from "react"
+
+function App() {
+  const [nombre, setNombre] = useState(0)
+  const [bolean, setBolean] = useState(true)
+
+  return (
+    <div className="App">
+      <p className={bolean? "green": "red"}>
+        {nombre}
+
+      </p>
+      <button onClick={()=>{
+       setNombre(nombre+1)
+      }}>
+        +1
+      </button>
+
+
+      <button onClick={()=>{
+       setNombre(nombre-1)
+      }}>
+        -1
+      </button>
+
+      <button onClick={()=>{
+        setBolean(!bolean)
+
+        
+      }}>rouge</button>
+
+    </div>
+  );
+}
+
+export default App;
